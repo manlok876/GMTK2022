e79f6b5ee8aa21175c14ac2fc3c09f315ba3bc2d
Denis Maximenko <d.maximenko@game-positive.com>
2022-07-15 18:48:15 +0300

    Blank project

---
+++ Source/GMTK2022/Public/GMTK2022GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GMTK2022GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GMTK2022_API AGMTK2022GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
