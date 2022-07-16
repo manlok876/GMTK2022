78eb5e6b0fc657529166ef1d1cad431dad4b34f9
Denis Maximenko <d.maximenko@game-positive.com>
2022-07-16 13:09:48 +0300

    Base figure class update, implement most of basic logic

--- Source/GMTK2022/Private/BaseChessFigure.cpp
+++ Source/GMTK2022/Private/BaseChessFigure.cpp
@@ -11,12 +11,26 @@ ABaseChessFigure::ABaseChessFigure()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	CachedGrid = nullptr;
+	bIsMoveInProgress = false;
+	PendingCombatTarget = nullptr;
 }
 
 void ABaseChessFigure::BeginPlay()
 {
 	Super::BeginPlay();
 
+}
+
+void ABaseChessFigure::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	AChessGrid* Grid = GetGrid();
+	if (IsValid(Grid))
+	{
+		Grid->FreeCell(this);
+	}
+
+	Super::EndPlay(EndPlayReason);
 }
 
 void ABaseChessFigure::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -33,20 +47,107 @@ void ABaseChessFigure::Tick(float DeltaTime)
 
 AChessGrid* ABaseChessFigure::GetGrid()
 {
-	return nullptr;
+	if (!IsValid(CachedGrid))
+	{
+		for (TActorIterator<AChessGrid> It(GetWorld()); It; ++It)
+		{
+			CachedGrid = *It;
+			break;
+		}
+	}
+
+	return CachedGrid;
 }
 
-TArray<FGridCoords> ABaseChessFigure::GetPossibleMoves() const
+TArray<FGridCoords> ABaseChessFigure::GetPossibleMoves_Implementation() const
 {
 	return TArray<FGridCoords>();
 }
 
-bool ABaseChessFigure::CanGoTo(const FGridCoords& Cell) const
+bool ABaseChessFigure::CanGoTo_Implementation(const FGridCoords& Cell) const
 {
 	return false;
 }
 
 bool ABaseChessFigure::TryGoTo(const FGridCoords& Cell)
 {
-	return false;
+	if (!CanGoTo(Cell))
+	{
+		return false;
+	}
+
+	bIsMoveInProgress = true;
+	PendingDestination = Cell;
+	StartMovementAnimation(Cell);
+
+	return true;
+}
+
+bool ABaseChessFigure::IsMoveInProgress() const
+{
+	return bIsMoveInProgress;
+}
+
+FGridCoords ABaseChessFigure::GetPendingDestination() const
+{
+	return PendingDestination;
+}
+
+ABaseChessFigure* ABaseChessFigure::GetPendingCombatTarget() const
+{
+	return PendingCombatTarget;
+}
+
+bool ABaseChessFigure::IsCombatPending() const
+{
+	return IsValid(GetPendingCombatTarget());
+}
+
+void ABaseChessFigure::PrepareToDefend_Implementation(ABaseChessFigure* Attacker)
+{
+	PendingCombatTarget = Attacker;
+}
+
+void ABaseChessFigure::ClearPendingCombat_Implementation()
+{
+	PendingCombatTarget = nullptr;
+}
+
+void ABaseChessFigure::EndMovementAnimation()
+{
+	if (IsCombatPending())
+	{
+		ABaseChessFigure* Target = GetPendingCombatTarget();
+		if (IsValid(Target))
+		{
+			StartCombatAnimation(Target, true);
+			Target->StartCombatAnimation(this, false);
+		}
+	}
+	else
+	{
+		EndMove();
+	}
+}
+
+void ABaseChessFigure::EndCombatAnimation()
+{
+	ClearPendingCombat();
+}
+
+void ABaseChessFigure::EndMove()
+{
+	AChessGrid* Grid = GetGrid();
+	if (IsValid(Grid))
+	{
+		Grid->FreeCell(this);
+		Grid->TryOccupyCell(this, GetPendingDestination());
+	}
+
+	bIsMoveInProgress = false;
+
+	if (MoveEndedDispatcher.IsBound())
+	{
+		MoveEndedDispatcher.Broadcast(this);
+	}
 }
--- Source/GMTK2022/Public/BaseChessFigure.h
+++ Source/GMTK2022/Public/BaseChessFigure.h
@@ -9,6 +9,8 @@
 
 class AChessGrid;
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FChessMoveEndedSignature, ABaseChessFigure*, MovedFigure);
+
 UCLASS()
 class GMTK2022_API ABaseChessFigure : public APawn
 {
@@ -20,20 +22,61 @@ public:
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	UFUNCTION(BlueprintCallable, BlueprintPure = false)
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, BlueprintPure = false)
 	TArray<FGridCoords> GetPossibleMoves() const;
-	UFUNCTION(BlueprintCallable, BlueprintPure = false)
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, BlueprintPure = false)
 	bool CanGoTo(const FGridCoords& Cell) const;
 	UFUNCTION(BlueprintCallable)
 	bool TryGoTo(const FGridCoords& Cell);
 
+	UFUNCTION(BlueprintPure)
+	bool IsMoveInProgress() const;
+	FGridCoords GetPendingDestination() const;
+
+	ABaseChessFigure* GetPendingCombatTarget() const;
+	UFUNCTION(BlueprintPure)
+	bool IsCombatPending() const;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void PrepareToDefend(ABaseChessFigure* Attacker);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void ClearPendingCombat();
+
+	UPROPERTY(BlueprintAssignable)
+	FChessMoveEndedSignature MoveEndedDispatcher;
+
 protected:
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
 	AChessGrid* GetGrid();
 
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartMovementAnimation(const FGridCoords& Cell);
+	UFUNCTION(BlueprintCallable)
+	void EndMovementAnimation();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartCombatAnimation(ABaseChessFigure* Target, bool bIsAttacker);
+	UFUNCTION(BlueprintCallable)
+	void EndCombatAnimation();
+
+	UFUNCTION()
+	void EndMove();
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnMoveEnded();
+
 private:
 	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-		AChessGrid* Grid;
+	AChessGrid* CachedGrid;
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	bool bIsMoveInProgress;
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FGridCoords PendingDestination;
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	ABaseChessFigure* PendingCombatTarget;
 
 };
